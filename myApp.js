391cc58efd4158478f31ca64d9abca8a044eb660
medinatochi <medinatoci@hotmail.com>
2022-01-22 22:45:55 +0100

    Chain Middleware fix

--- myApp.js
+++ myApp.js
@@ -19,6 +19,7 @@ app.get("/json", function(req, res){
 
 app.get("/now", function(req, res, next){
     req.time = new Date().toString();
+    next();
 }, function(req, res){
     res.json({"time":req.time});
 })
