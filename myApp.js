a8e92c1aac4e9b91998eb172895c6450e5edcfad
medinatochi <medinatoci@hotmail.com>
2022-01-23 10:59:55 +0100

    data from post requests

--- myApp.js
+++ myApp.js
@@ -36,7 +36,7 @@ app.get("/:word/echo", function(req, res){
 app.route("/name").get(function(req, res){
     res.json({"name": req.query.first + " " + req.query.last})
 }).post(function(req, res){
-
+    res.json({"name": req.body.first + " " + req.body.last})
 })
 
 var absolutePathAssets = __dirname + "/public";
